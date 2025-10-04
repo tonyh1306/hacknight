52dcb62b024007d85c0b33d44e71120a0f2b51e1
Rae <raegedangoni@gmail.com>
2025-10-04 15:34:59 -0400

    new prompt

--- nutrilens/app/scan/food/page.tsx
+++ nutrilens/app/scan/food/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import type React from "react"
+import React from "react"
 import { Navigation } from "@/components/navigation"
 import { Footer } from "@/components/footer"
 import { useState } from "react"
